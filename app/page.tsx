d7f34c7f82a99a4a2a0187a5338c0eec4b61ba94
alessandroamenta <alessandroamenta1@gmail.com>
2024-05-17 17:59:59 +0200

    Refactor Spline component layout

--- app/page.tsx
+++ app/page.tsx
@@ -67,8 +67,10 @@ export default function Home() {
           </div>
         </header>
         <div style={{ position: 'relative' }} >
-          <div style={{ height: 600 }}>
-            <Spline scene="https://prod.spline.design/P63ncCY2mpMOKsJf/scene.splinecode" />
+          <div style={{ position: 'relative', width: '100%', paddingTop: '60%' }}>
+            <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+              <Spline scene="https://prod.spline.design/P63ncCY2mpMOKsJf/scene.splinecode" />
+            </div>
           </div>
           <main className="flex-1 container mx-auto px-4 md:px-6 relative top-[-230px]">
             <div className="flex flex-col items-center">
