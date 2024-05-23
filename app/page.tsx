8124ff1ceeeccad5bdad1164d1e7c359ddf184e9
alessandroamenta <alessandroamenta1@gmail.com>
2024-05-23 14:49:14 +0200

    Refactor page layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -109,16 +109,17 @@ export default function Home() {
             </div>
           </div>
         </header>
-        <div style={{ position: 'relative' }}>
+        <div className="container mx-auto px-4 md:px-6 mt-8">
           <div
-            className="text-overlay absolute top-[10%] left-[33%] text-left hidden sm:block"
+            className="text-overlay text-left hidden sm:block"
             style={{
               fontSize: '2.5vw',
               fontFamily: 'monospace',
               lineHeight: '0.9',
               textAlign: 'left',
-              top: '8%',
-              left: '33%'
+              marginBottom: '2rem',
+              top: '26%',
+              left: '39%'
             }}
           >
             <h1 style={{ marginBottom: '0.5rem' }}>
@@ -128,55 +129,59 @@ export default function Home() {
               theaieng.com is the only job board<br />explicitly dedicated to AI Engineering roles.
             </p>
           </div>
-          <div style={{ position: 'relative', width: '100%', paddingTop: '55%' }}>
+        </div>
+        <div style={{ position: 'relative' }}>
+          <div style={{ position: 'relative', width: '100%', paddingTop: '50%' }}>
             <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
               <Spline scene="https://prod.spline.design/LEETA5bmC7fHpeZN/scene.splinecode" />
             </div>
           </div>
-          <main className="flex-1 container mx-auto px-4 md:px-6 relative top-[-230px] md:top-[-210px]">
-            <div className="flex flex-col items-center">
-              <div className="flex justify-between items-center mb-6 w-full max-w-2xl">
-                <div className="flex items-center gap-4 rounded-md">
-                  <Button
-                    variant="outline"
-                    className="rounded-md bg-[#090909] border border-[#181818]"
-                    style={{ pointerEvents: 'none' }}
-                  >
-                    Count · {jobCount}
-                  </Button>
-                </div>
-              </div>
-              <div className="grid gap-2 w-full max-w-2xl mb-[-190px] md:mb-190">
-                {isLoading ? (
-                  Array.from({ length: showCount }).map((_, index) => (
-                    <SkeletonJobCard key={index} />
-                  ))
-                ) : (
-                  jobs.slice(0, showCount).map((job) => (
-                    <JobCard
-                      key={job.id}
-                      job_link={job.job_link}
-                      job_title={job.job_title}
-                      company_name={job.company_name}
-                      company_logo={job.company_logo}
-                      location={job.location}
-                      date_posted={job.date_posted}
-                      salary_range={job.salary_range}
-                    />
-                  ))
-                )}
-                {jobs.length > 20 && (
-                  <div className="flex justify-center mt-4">
+          <main className="flex-1 container mx-auto px-4 md:px-6 mt-[-200px] md:mt-[-250px]">
+            <div className="mt-8 md:mt-12 flex justify-center">
+              <div className="w-full max-w-2xl">
+                <div className="flex justify-between items-center mb-6">
+                  <div className="flex items-center gap-4 rounded-md relative z-10">
                     <Button
                       variant="outline"
-                      onClick={handleShowMore}
                       className="rounded-md bg-[#090909] border border-[#181818]"
-                      style={{ width: '120px' }}
+                      style={{ pointerEvents: 'none' }}
                     >
-                      {showCount === jobs.length ? 'Show Less' : 'Show More'}
+                      Count · {jobCount}
                     </Button>
                   </div>
-                )}
+                </div>
+                <div className="grid gap-2 relative z-0">
+                  {isLoading ? (
+                    Array.from({ length: showCount }).map((_, index) => (
+                      <SkeletonJobCard key={index} />
+                    ))
+                  ) : (
+                    jobs.slice(0, showCount).map((job) => (
+                      <JobCard
+                        key={job.id}
+                        job_link={job.job_link}
+                        job_title={job.job_title}
+                        company_name={job.company_name}
+                        company_logo={job.company_logo}
+                        location={job.location}
+                        date_posted={job.date_posted}
+                        salary_range={job.salary_range}
+                      />
+                    ))
+                  )}
+                  {jobs.length > 20 && (
+                    <div className="flex justify-center mt-3 mb-4">
+                      <Button
+                        variant="outline"
+                        onClick={handleShowMore}
+                        className="rounded-md bg-[#090909] border border-[#181818]"
+                        style={{ width: '120px' }}
+                      >
+                        {showCount === jobs.length ? 'Show Less' : 'Show More'}
+                      </Button>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </main>
